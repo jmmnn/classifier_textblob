e7c14163eeb69a47d4daa7e8accbb813797608be
jmmnn_1 <jmmnn@yahoo.com>
2016-12-06 21:38:12 +0000

    V1, working! added classifier using gutenberd and abc corpus

--- classifyer_dev.py
+++ classifyer_dev.py
@@ -7,15 +7,24 @@ from nltk.corpus import abc as corpus
 
 # lenght = len (corpus.raw())
 # print (corpus.readme())
-#print (gutenberg.fileids())
+print (gutenberg.fileids())
 #print (corpus.fileids())
 #head = corpus.raw('science.txt')
+train = []
+for sent in corpus.sents('science.txt')[50:350]:
+    train.append((' '.join(sent) , 'y'))
 
-# for sent in corpus.sents('science.txt'):
-#     print ("new_____" , sent)
+for sent in gutenberg.sents('austen-emma.txt')[50:350]:
+    train.append((' '.join(sent) , 'n'))
+    #print ("new_____" , ' '.join(sent))
+#print (train2)
 # # print (corpus.words('science.txt'))
 
-
+test = []
+for sent in corpus.sents('science.txt')[400:420]:
+    test.append((' '.join(sent) , 'y'))
+for sent in gutenberg.sents('austen-emma.txt')[400:420]:
+    test.append((' '.join(sent) , 'n'))
 
 # if "words" in head: 
 #   print ('success')
@@ -25,32 +34,32 @@ from nltk.corpus import abc as corpus
 
 #poverty trainer
 
-train = [
-    ('I am poor.', 'y'),
-    ('This society is in famine and poor', 'y'),
-    ('Hot weather is affecting the poor economy', 'y'),
-    ('This poor condition is lamentable.', 'y'),
-    ("What hunger in this poor town", 'y'),
-    ('I do not like this poor weather', 'y'),
-    ('I do have money.', 'y'),
-    ("The weather is so warm", 'n'),
-    ('The food was lacking', 'n'),
-    ('My neighbor is rich.', 'n')
-]
-test = [
-    ('The poor suffer from lack of money.', 'y'),
-    ('Tha weather is getting hot', 'n'),
-    ("the lack of food is overwhelming.", 'n'),
-    ("I don't have money", 'y'),
-    ('Gary does not have any food', 'n'),
-    ("Global climate weather change is happening.", 'n')
-]
+# train = [
+#     ('I am poor.', 'y'),
+#     ('This society is in famine and poor', 'y'),
+#     ('Hot weather is affecting the poor economy', 'y'),
+#     ('This poor condition is lamentable.', 'y'),
+#     ("What hunger in this poor town", 'y'),
+#     ('I do not like this poor weather', 'y'),
+#     ('I do have money.', 'y'),
+#     ("The weather is so warm", 'n'),
+#     ('The food was lacking', 'n'),
+#     ('My neighbor is rich.', 'n')
+# ]
+# test = [
+#     ('The poor suffer from lack of money.', 'y'),
+#     ('Tha weather is getting hot', 'n'),
+#     ("the lack of food is overwhelming.", 'n'),
+#     ("I don't have money", 'y'),
+#     ('Gary does not have any food', 'n'),
+#     ("Global climate weather change is happening.", 'n')
+# ]
 
 cl = NaiveBayesClassifier(train)
 
 # Classify some text
-print(cl.classify("Their village was so poor"))  # "y"
-print(cl.classify("I have my sandals"))   # "n"
+print(cl.classify("Thanks to a tiny goggle-wearing parrot named after Obi-Wan Kenobi, scientists have shown just how much we still don’t know about animal flight. The researchers set out to illustrate a problem that’s been warned of in their field of study, but never tested"))  # "y"
+print(cl.classify("Leave it to Gap to portray utility dressing as an art form. In recognition of World AIDS Day on December 1, the San Francisco-based brand has commissioned six international female street artists to reinvent its signature utility jacket by superimposing their own unique visions onto it."))   # "n"
 
 # # Classify a TextBlob
 # blob = TextBlob("The poor weather was increasing in temperature. There were people without food. "
@@ -62,8 +71,8 @@ print(cl.classify("I have my sandals"))   # "n"
 #     print(sentence)
 #     print(sentence.classify())
 
-# # Compute accuracy
-# print("Accuracy: {0}".format(cl.accuracy(test)))
+# Compute accuracy
+print("Accuracy: {0}".format(cl.accuracy(test)))
 
-# # Show 5 most informative features
-# cl.show_informative_features(5)
+# Show 5 most informative features
+cl.show_informative_features(5)
